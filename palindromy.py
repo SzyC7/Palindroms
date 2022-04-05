85f5f93234b4dd61cf894c6a497aad31dd00fa68
SzyC7 <101209123+SzyC7@users.noreply.github.com>
2022-04-05 17:35:06 +0200

    Update palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -1,11 +1,12 @@
-def czyPalindrom(x):
-    x = x.lower().replace(" ", "") #zmieniamy wszystkie literki na male i pozbywamy sie spacji
+def ifPalindrom(x):
+    x = "word"
+    print(x[::-1]) #zmieniamy wszystkie literki na male i pozbywamy sie spacji
     n = len(x) #zmienna pomocnicza przechowujaca dlugosc slowa
     for i in range(n-1):
         if x[i] != x[n-1-i]: #jezeli znak po przeciwnej stronie (w tej samej kolejnosci od konca) nie bedzie taki sam
             return False #zwroc false
-    return True; #jezeli nie napotkano problemow, zwroc true
+    return True #jezeli nie napotkano problemow, zwroc true
 print("Program sprawdzajacy czy slowo jest palindromem")
 print("Podaj slowo")
 s1 = input() #pobieramy slowo
-print("Podane slowo " + ("jest " if(czyPalindrom(s1)) else "nie jest ") + "palindromem")
+print("Podane slowo " + ("jest " if ifPalindrom(s1) else "nie jest ") + "palindromem")
